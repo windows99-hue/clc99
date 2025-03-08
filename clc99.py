6d296b8805933e694964d96a8428a07b6425a366
windows99-hue <3013907412@qq.com>
2025-03-08 22:15:22 +0800

    让每个函数的标志和文字间隔一个空格

--- clc99.py
+++ clc99.py
@@ -29,144 +29,144 @@ def initsystem():
 
 def print_status(*args, full=False, end="\n"):
     if full:
-        print(Fore.BLUE + '[*]', end='')
+        print(Fore.BLUE + '[*]', end=' ')
         print(*args, end=end)
     else:
-        print(Fore.BLUE + '[*]' + Fore.RESET, end='')
+        print(Fore.BLUE + '[*]' + Fore.RESET, end=' ')
         print(*args, end=end)
 
 def print_good(*args,full=False,end="\n"):
     if full:
-        print(Fore.BLUE+'[+]',end='')
+        print(Fore.BLUE+'[+]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.BLUE+'[+]'+Fore.RESET,end='')
+        print(Fore.BLUE+'[+]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_error(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[-]',end='')
+        print(Fore.RED+'[-]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[-]'+Fore.RESET,end='')
+        print(Fore.RED+'[-]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_warning(*args,full=False,end="\n"):
     if full:
-        print(Fore.YELLOW+'[!]',end='')
+        print(Fore.YELLOW+'[!]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.YELLOW+'[!]'+Fore.RESET,end='')
+        print(Fore.YELLOW+'[!]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_finish(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[√]',end='')
+        print(Fore.GREEN+'[√]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[√]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[√]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_os(*args,full=False,end="\n"):
     if full:
-        print(Fore.CYAN+'[$]',end='')
+        print(Fore.CYAN+'[$]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.CYAN+'[$]'+Fore.RESET,end='')
+        print(Fore.CYAN+'[$]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_notrun(*args,full=False,end="\n"):
     if full:
-        print(Fore.MAGENTA+'[#]',end='')
+        print(Fore.MAGENTA+'[#]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.MAGENTA+'[#]'+Fore.RESET,end='')
+        print(Fore.MAGENTA+'[#]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_e(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[ERROR]',end='')
+        print(Fore.RED+'[ERROR]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[ERROR]'+Fore.RESET,end='')
+        print(Fore.RED+'[ERROR]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_fileok(*args,full=False,end="\n"):
     if full:
-        print(Fore.BLUE+'[.]',end='')
+        print(Fore.BLUE+'[.]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.BLUE+'[.]'+Fore.RESET,end='')
+        print(Fore.BLUE+'[.]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_filerror(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[.]',end='')
+        print(Fore.RED+'[.]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[.]'+Fore.RESET,end='')
+        print(Fore.RED+'[.]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_time(str='',shijiangeshi="%Y-%m-%d %H:%M:%S",title='front',full=False,end="\n"):
     if full:
         if title == 'front':
-            print(Fore.CYAN+'[TIME]',end='')
+            print(Fore.CYAN+'[TIME]',end=' ')
             print(str+time.strftime(shijiangeshi, time.localtime()))
         if title == 'before':
-            print(Fore.CYAN+'[TIME]',end='')
+            print(Fore.CYAN+'[TIME]',end=' ')
             print(time.strftime(shijiangeshi, time.localtime())+str)
         if title == 'middle':
-            print(Fore.CYAN+'[TIME]',end='')
+            print(Fore.CYAN+'[TIME]',end=' ')
             print(str+time.strftime(shijiangeshi, time.localtime())+str)
     else:
         if title == 'front':
-            print(Fore.CYAN+'[TIME]'+Fore.RESET,end='')
+            print(Fore.CYAN+'[TIME]'+Fore.RESET,end=' ')
             print(str+time.strftime(shijiangeshi, time.localtime()))
         if title == 'before':
-            print(Fore.CYAN+'[TIME]'+Fore.RESET,end='')
+            print(Fore.CYAN+'[TIME]'+Fore.RESET,end=' ')
             print(time.strftime(shijiangeshi, time.localtime())+str)
         if title == 'middle':
-            print(Fore.CYAN+'[TIME]'+Fore.RESET,end='')
+            print(Fore.CYAN+'[TIME]'+Fore.RESET,end=' ')
             print(str+time.strftime(shijiangeshi, time.localtime())+str)
 
 def print_music(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[playmusic]',end='')
+        print(Fore.GREEN+'[playmusic]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[playmusic]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[playmusic]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_video(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[playvideo]',end='')
+        print(Fore.GREEN+'[playvideo]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[playvideo]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[playvideo]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_ok(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[OK]',end='')
+        print(Fore.GREEN+'[OK]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[OK]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[OK]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_over(*args,full=False,end="\n"):
     if full:
-        print(Fore.WHITE+'[OVER]',end='')
+        print(Fore.WHITE+'[OVER]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.WHITE+'[OVER]'+Fore.RESET,end='')
+        print(Fore.WHITE+'[OVER]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_admin(*args,full=False,end="\n"):
     if full:
-        print(Fore.CYAN+'[Admin]',end='')
+        print(Fore.CYAN+'[Admin]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.CYAN+'[Admin]'+Fore.RESET,end='')
+        print(Fore.CYAN+'[Admin]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def input_str(str,full=False):
@@ -179,50 +179,50 @@ def input_str(str,full=False):
     
 def print_dirok(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[/]',end='')
+        print(Fore.GREEN+'[/]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[/]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[/]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_direrror(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[/]',end='')
+        print(Fore.RED+'[/]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[/]'+Fore.RESET,end='')
+        print(Fore.RED+'[/]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_comok(*args,full=False,end="\n"):
     if full:
-        print(Fore.GREEN+'[C]',end='')
+        print(Fore.GREEN+'[C]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.GREEN+'[C]'+Fore.RESET,end='')
+        print(Fore.GREEN+'[C]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_comerror(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[C]',end='')
+        print(Fore.RED+'[C]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[C]'+Fore.RESET,end='')
+        print(Fore.RED+'[C]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_uquestion(*args,full=False,end="\n"):
     if full:
-        print(Fore.YELLOW+'[?]',end='')
+        print(Fore.YELLOW+'[?]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.YELLOW+'[?]'+Fore.RESET,end='')
+        print(Fore.YELLOW+'[?]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def print_cquestion(*args,full=False,end="\n"):
     if full:
-        print(Fore.RED+'[?]',end='')
+        print(Fore.RED+'[?]',end=' ')
         print(*args, end=end)
     else:
-        print(Fore.RED+'[?]'+Fore.RESET,end='')
+        print(Fore.RED+'[?]'+Fore.RESET,end=' ')
         print(*args, end=end)
 
 def user_color(title,color,full=False):
